18e806e3488461277fda164372e9ee96f762487c
John Kleinschmidt <jk@cure.org>
2014-04-08 16:02:49 -0400

    Fixed issue proxying DELETE actions

--- forward.js
+++ forward.js
@@ -3,12 +3,14 @@ var request = require('request');
 module.exports = function(pattern, host){
     return function(req, res, next){
         if(req.url.match(pattern)){
-            var db_path = req.url.match(pattern)[1]
-                , db_url = [host, db_path].join('/');
-            console.log("proxying to: "+db_url);
-            req.pipe(request[req.method.toLowerCase()](db_url)).pipe(res);
+            var db_path = req.url.match(pattern)[1],
+                db_url = [host, db_path].join('/'),
+                req_method = req.method.toLowerCase();
+            if (req_method == 'delete') {
+                req_method = 'del';
+            }
+            req.pipe(request[req_method](db_url)).pipe(res);
         } else {
-            console.log("skipping: "+req.url);
             next();
         }
     }
