f1c69714da7361c2cd81b67b86fd9220c9ece540
John Kleinschmidt <kleinschmidtorama@gmail.com>
2015-05-19 16:10:54 -0400

    Fixed passing in couch credentials.

--- utils/elasticsearch.sh
+++ utils/elasticsearch.sh
@@ -45,24 +45,24 @@ curl -XPUT 'localhost:9200/hrdb' -d' {
     }
  }'
 echo "Setting up couchdb river" 
-curl -XPUT 'localhost:9200/_river/hrdb/_meta' -d '{
-    "type" : "couchdb",
-    "couchdb" : {
-        "host" : "localhost",
-        "port" : 5984,
-        "db" : "main",
-        "filter" : null,
-        "user" : $1,
-        "password" : $2,
-        "ignore_attachments": true,
-        "script_type": "js",
-        "script": "var uidx = ctx.doc._id.indexOf(\"_\");if (ctx.doc._id && (uidx = ctx.doc._id.indexOf(\"_\")) > 0) {    ctx._type = ctx.doc._id.substring(0, uidx);} var fieldsToKeep = [];switch(ctx._type) {     case \"inventory\": {        fieldsToKeep = [\"crossReference\",\"description\",\"friendlyId\",\"name\"];                break;    }     case \"invoice\" : {         fieldsToKeep = [\"patientInfo\", \"externalInvoiceNumber\"];        break;    }    case \"patient\" : {         fieldsToKeep = [\"externalPatientId\",\"firstName\",\"friendlyId\",\"lastName\"];        break;    }     case \"pricing\" : {        fieldsToKeep = [\"name\"];                    break;    }     default: { ctx.ignore = true; }}"
+curl -XPUT 'localhost:9200/_river/hrdb/_meta' -d "{
+    \"type\" : \"couchdb\",
+    \"couchdb\" : {
+        \"host\" : \"localhost\",
+        \"port\" : 5984,
+        \"db\" : \"main\",
+        \"filter\" : null,
+        \"user\" : \"$1\",
+        \"password\" : \"$2\",
+        \"ignore_attachments\": true,
+        \"script_type\": \"js\",
+        \"script\": \"var uidx = ctx.doc._id.indexOf(\\\"_\\\");if (ctx.doc._id && (uidx = ctx.doc._id.indexOf(\\\"_\\\")) > 0) {    ctx._type = ctx.doc._id.substring(0, uidx);} var fieldsToKeep = [];switch(ctx._type) {     case \\\"inventory\\\": {        fieldsToKeep = [\\\"crossReference\\\",\\\"description\\\",\\\"friendlyId\\\",\\\"name\\\"];                break;    }     case \\\"invoice\\\" : {         fieldsToKeep = [\\\"patientInfo\\\", \\\"externalInvoiceNumber\\\"];        break;    }    case \\\"patient\\\" : {         fieldsToKeep = [\\\"externalPatientId\\\",\\\"firstName\\\",\\\"friendlyId\\\",\\\"lastName\\\"];        break;    }     case \\\"pricing\\\" : {        fieldsToKeep = [\\\"name\\\"];                    break;    }     default: { ctx.ignore = true; }}\"
     },
-    "index" : {
-        "index" : "hrdb",
-        "type" : "hrdb",
-        "bulk_size" : "100",
-        "bulk_timeout" : "10ms"
+    \"index\" : {
+        \"index\" : \"hrdb\",
+        \"type\" : \"hrdb\",
+        \"bulk_size\" : \"100\",
+        \"bulk_timeout\" : \"10ms\"
     }
-}'
+}"
 echo "Done"
