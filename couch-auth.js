645daae82f0237e40682fafe9e0dc98e5e4b0932
John Kleinschmidt <kleinschmidtorama@gmail.com>
2016-01-29 13:54:55 -0500

    Removed no longer needed crud routes
    Closes #3

--- couch-auth.js
+++ couch-auth.js
@@ -1,22 +1,22 @@
-var config =  require('./config.js'), 
+var config =  require('./config.js'),
     nano = require('nano')(config.couch_auth_db_url),
     serializer = require('serializer'),
-    users = nano.use('_users');  
+    users = nano.use('_users');
 
 function create_oauth_tokens(secret_base, user, callback) {
     var consumer_key = serializer.randomString(96);
-    var token_key = serializer.randomString(96);    
+    var token_key = serializer.randomString(96);
     user.oauth = {
         consumer_keys: {},
         tokens: {}
     };
-    user.oauth.consumer_keys[consumer_key] = create_secret(secret_base);    
+    user.oauth.consumer_keys[consumer_key] = create_secret(secret_base);
     user.oauth.tokens[token_key] = create_secret(secret_base);
     users.insert(user, user._id, function(err, response) {
         if (err || !response.ok) {
             console.log("ERROR Updating user with credentials:");
             console.dir(response);
-            callback(response);            
+            callback(response);
         } else {
             callback(null, denormalize_oauth(user));
         }
@@ -28,7 +28,7 @@ function create_secret(secret_base) {
     var validate_key = serializer.randomString(48);
     var secret_string = serializer.secureStringify(secret_base, encrypt_key, validate_key);
     if (secret_string.length > 80) {
-        secret_string = secret_string.substr(30,50);    
+        secret_string = secret_string.substr(30,50);
     }
     return secret_string;
 }
@@ -37,27 +37,15 @@ function denormalize_oauth(user) {
     var key;
     for (key in user.oauth.consumer_keys) {
         user.consumer_key = key;
-        user.consumer_secret = user.oauth.consumer_keys[key];        
+        user.consumer_secret = user.oauth.consumer_keys[key];
         break;
     }
     for (key in user.oauth.tokens) {
-        user.token_key = key;        
+        user.token_key = key;
         user.token_secret = user.oauth.tokens[key];
         break;
     }
     return user;
-}
-
-function is_admin(user) {
-    var isAdmin = false;
-    if (user.roles) {
-        user.roles.forEach(function(role) {
-            if (role === 'admin') {
-                isAdmin = true;
-            }
-        });
-    }
-    return isAdmin;
 }
 
 function validate_oauth(oauth) {
@@ -70,20 +58,8 @@ function validate_oauth(oauth) {
     }
     return false;
 }
-                                
+
 module.exports = {
-    delete_user: function(user, idToDelete, rev, res) {
-        if(is_admin(user)) {
-            users.destroy(idToDelete, rev, function(err, body) {
-                if (err) {
-                    res.json({error:true, errorResult: err});
-                } else {
-                    res.json(body);
-                }
-            });
-        }
-    },
-    
     find_oauth_user: function(accessToken, refreshToken, profile, callback) {
         var user_key = 'org.couchdb.user:'+profile.emails[0].value;
         users.get(user_key, {}, function(err, body) {
@@ -94,59 +70,29 @@ module.exports = {
                     callback(err);
                 }
                 return;
-            }        
+            }
             if (validate_oauth(body.oauth)) {
-                return callback(null, denormalize_oauth(body));            
+                return callback(null, denormalize_oauth(body));
             } else {
                 create_oauth_tokens(accessToken, body, callback);
             }
         });
     },
-    
+
     find_user: function(userName, callback) {
         var user_key = userName;
         if (user_key.indexOf('org.couchdb.user:') !== 0) {
             user_key = 'org.couchdb.user:' + user_key;
-        }      
+        }
         users.get(user_key, {}, function(err, body) {
             if (err) {
                 callback(err);
                 return;
-            }        
+            }
             callback(null, body);
-        });    
+        });
     },
-    
-    get_user: function(user, id, res) {
-        if(is_admin(user)) {
-            this.find_user(id, function(err, body) {
-                if (err) {
-                    res.json({error:true, errorResult: err});
-                } else {
-                    res.json(body);
-                }
-            });
-        }
-    },
-    
-    get_users: function(user, res) {
-        if(is_admin(user)) {
-            var options = {
-                include_docs: true,
-                startkey: 'org.couchdb.user'
-            };
-            users.list(options, function (err, body) {
-                if (err) {
-                    res.json({error:true, errorResult: err});
-                } else {
-                    res.json(body);
-                }
-            });
-        } else {
-            res.json({error:true, errorResult: 'Unauthorized'});
-        }
-    },
-    
+
     get_primary_role: function(user) {
         var primaryRole = '';
         if (user.roles) {
@@ -157,18 +103,6 @@ module.exports = {
             });
         }
         return primaryRole;
-    },
-    
-    update_user: function(user, userData, updateParams, res) {
-        if(is_admin(user)) {
-            users.insert(userData, updateParams, function(err, body) {
-                if (err) {
-                    res.json({error:true, errorResult: err});
-                } else {
-                    res.json(body);
-                }
-            });
-        }
-    },
+    }
 
 };
